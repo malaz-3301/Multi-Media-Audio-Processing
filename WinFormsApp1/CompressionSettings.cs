1f120f725f9f38fff57351dc5ea0212dcf2e70db
Mohammad Malaz Ahmad <lozo4hk@gmail.com>
2026-06-08 08:22:09 +0300

    fix

--- WinFormsApp1/CompressionSettings.cs
+++ WinFormsApp1/CompressionSettings.cs
@@ -2,22 +2,10 @@ using WinFormsApp1;
 
 public class CompressionSettings
 {
-    private float stepSize;
-
-    public static float CurrentDeltaStepSize { get; private set; } = 0.01f;
     public CompressionTypes Type { get; set; }
     public int SampleRate { get; set; }
     public int QuantizationLevels { get; set; }
     public string SavePath { get; set; }
     public float QuantizationFactor { get; set; }
-
-    public float StepSize
-    {
-        get => stepSize;
-        set
-        {
-            stepSize = value <= 0 ? 0.01f : value;
-            CurrentDeltaStepSize = stepSize;
-        }
-    }
+    public float StepSize { get; set; } = 0.01f;
 }
